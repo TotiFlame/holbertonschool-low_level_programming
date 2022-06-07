2e7d6c84840ac58c3469a49aba7b59f7cd98c750
TotiFlame <santifer4321@gmail.com>
2022-06-07 08:29:27 -0700

    up to date

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a' ; alp <= 'z' ; alp++)
+	{
+		if (alp != 'e' || alp != 'q')
+		{
+		putchar(alp);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
