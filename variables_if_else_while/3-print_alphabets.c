9f4d173f46d6c9b757b1ece7ebc8c97e51aa0775
TotiFlame <santifer4321@gmail.com>
2022-06-07 08:16:42 -0700

    up to date

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alp;
+	char alpu;
+
+	for (alp = 'a' ; alp <= 'z' ; alp++)
+	{
+	putchar(alp);
+	}
+	
+	for (alpu = 'A' ; alpu <= 'Z' ; alpu++)
+	{
+	putchar(alpu);
+	}
+	putchar('\n');
+
+	return (0);
+}
