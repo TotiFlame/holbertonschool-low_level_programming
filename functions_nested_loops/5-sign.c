239be9e0969aeb97258b0759eb9a5fd3ab997f62
TotiFlame <santifer4321@gmail.com>
2022-06-09 09:52:24 -0700

    commit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_sign - print the number's sign
+* @n: character
+* Return: Always 1
+*
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
+
+}
