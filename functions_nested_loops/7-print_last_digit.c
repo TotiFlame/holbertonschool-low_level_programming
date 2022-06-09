28526a591d52144690eccca49da1e1e714d76d09
TotiFlame <santifer4321@gmail.com>
2022-06-09 11:21:04 -0700

    commit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
 * print_last_digit - print last digit of a number
@@ -16,5 +17,5 @@ int print_last_digit(int n)
 
 	_putchar(abs(ld) + '0');
 	return (abs(ld));
-
+	_putchar('\n');
 }
