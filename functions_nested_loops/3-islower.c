d98328c93ca795b6d24cbd413251890dac3e7b4f
TotiFlame <santifer4321@gmail.com>
2022-06-09 07:19:03 -0700

    commit

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,26 +1,17 @@
 #include "main.h"
 
 /**
-*
 * _islower - return if the number is lower or not
 *
-* return: 0 or 1.
+* @c: character
 *
+* return: 0 or 1.
 */
 
 int _islower(int c)
 {
-	int i;
-
-	for (i = 97; i <= 122; i++)
-	{
-		if (c == i)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-	}
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
