5ec2dd4698149143ef2f9b234bfffccb98c0b2a5
TotiFlame <santifer4321@gmail.com>
2022-06-09 11:57:23 -0700

    up to date

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -15,15 +15,15 @@ void jack_bauer(void)
 
 	for (h = 0; h < 24; h++)
 	{
-		_putchar((h/10)+'0');
-		_putchar((h%10)+'0');
+		_putchar((h / 10) + '0');
+		_putchar((h % 10) + '0');
 	}
-	
-	_putcha(':');
+
+	_putchar(':');
 
 	for (m = 0; m < 60; m++)
 	{
-		_putchar((m/10)+'0');
-		_putchar((m%10)+'0');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
 	}
 }
