fbc1c8a7c4fe87a0bfd31be257a094615838d9d9
TotiFlame <santifer4321@gmail.com>
2022-06-10 06:35:03 -0700

    commit

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* times_table - prints the 9 times table
+*
+* Return: 1
+*/
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int r = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+			    {
+					
+					r = i * j;
+					if (r <= 9)
+					{
+						_putchar(' ');
+						_putchar((r%10) + '0');
+					}
+					else
+					{
+					_putchar((r/10) + '0');
+					_putchar((r%10)+'0');
+					}
+					if (j != 9)
+					{
+					_putchar(',');
+					_putchar(' ');
+					}
+				}
+		_putchar('\n');
+	}
+
+}
