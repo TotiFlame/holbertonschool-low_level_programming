3a235b7873e9b3e16c2c65200b4b3b540059e453
TotiFlame <santifer4321@gmail.com>
2022-06-10 09:49:11 -0700

    commit

---
+++ debugging/positive_or_negative.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+*positive_or_negative - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+			printf("%d is positive\n", i);
+	} else if (n == 0)
+	{
+			printf("%d is zero\n", i);
+	} else
+	{
+			printf("%d is negative\n", i);
+	}
+	return (0);
+}
