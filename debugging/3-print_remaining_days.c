3ee21323371f4202f155f69a9a23081140515486
TotiFlame <santifer4321@gmail.com>
2022-06-11 13:47:22 -0700

    commit

--- debugging/3-print_remaining_days.c
+++ debugging/3-print_remaining_days.c
@@ -18,7 +18,7 @@ if (year % 4 == 0)
 	{
 	day++;
 	printf("Day of the year: %d\n", day - 1);
-	printf("Remaining days: %d\n", 366 - day);
+	printf("Remaining days: %d\n", 366 - day + 1);
 	}
 	else if (month >= 2 && day >= 60)
 	{
