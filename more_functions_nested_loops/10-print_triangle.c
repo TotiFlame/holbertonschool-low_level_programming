56929063a7b6d054d569de66be8697d9fb9141d8
TotiFlame <santifer4321@gmail.com>
2022-06-12 09:29:46 -0700

    commit

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* print_triangle - draws a square in the terminal
+* @size: number
+*
+* Return: void
+*/
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int n;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j <= size; j++)
+			{
+				_putchar(' ');
+			}
+		_putchar('#');
+		_putchar('\n');
+		}
+	}
+}
