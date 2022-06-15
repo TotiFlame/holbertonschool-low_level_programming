c8e79b877017daebdeac8d94e1e2f11c6a99c2fd
TotiFlame <santifer4321@gmail.com>
2022-06-15 14:18:59 -0700

    commit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_array - prints n elements of an array of integers
+* @a: integer pointer
+* @n: integer
+*
+*/
+
+void print_array(int *a, int n)
+{
+	int cont;
+
+	for (cont = 0; cont <= n; cont++)
+	{
+		if (cont != n)
+		{
+			printf("%d, ", a[cont]);
+		}
+		else
+		{
+			printf("%d", a[cont]);
+		}
+	}
+
+	printf("\n");
+}
