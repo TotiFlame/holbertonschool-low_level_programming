a052dd6bf540ba76c8d55ddb9734c4bfc15f1734
TotiFlame <santifer4321@gmail.com>
2022-06-16 11:51:31 -0700

    commit

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,12 +13,12 @@ char *_strcat(char *dest, char *src)
 	int dlen;
 	int scon;
 
-	while (dest[dlen])
+	while (dest[dlen] != '\0')
 	{
 		dlen++;
 	}
 
-	while (src[scon])
+	while (src[scon] != '\0')
 	{
 		scon++;
 		dest[dlen] = src[scon];
