7f6b4e6eab5cc3b5af4d797d2acf2d46c8437d83
TotiFlame <santifer4321@gmail.com>
2022-06-16 12:41:13 -0700

    up to date

---
+++ pointers_arrays_strings/1-strncar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strncat - concatenates two strings
+* @dest: pointer
+* @src: pointer
+* @n: integer
+* Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen;
+	int scon;
+
+	for (dlen = 0; dest[dlen] != '\0'; dlen++)
+	{
+	}
+
+	for (scon = 0; scon <= n; scon++)
+	{
+		dest[dlen] = src[scon];
+		dlen++;
+	}
+
+	return (dest);
+}
