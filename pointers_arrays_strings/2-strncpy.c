0871daf3a7f8f30d80c4e323488249d2469238d8
TotiFlame <santifer4321@gmail.com>
2022-06-17 08:33:42 -0700

    commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int cont;
 
-	for (cont = 0; cont < n; cont++)
+	for (cont = 0; cont < n, dest[n], src[n]; cont++)
 	{
-		if (src[0] != '\0')
-		{
-			dest[cont] = src[cont];
-		}
+		
 	}
 	return (dest);
 }
