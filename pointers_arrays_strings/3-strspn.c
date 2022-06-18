d48315dfe3f9bf298ea44d32ad2de20bfc922ed8
TotiFlame <santifer4321@gmail.com>
2022-06-17 23:17:02 -0700

    up to date

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,14 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int scont;
 	int acont;
-	//int cant;
 	unsigned int res = 0;
-
-	/**
-	*for (cant = 0; accept[cant] != '\0'; cant++)
-	*{
-	*}
-	*/
 
 	for (scont = 0; scont <= 5; scont++)
 	{
