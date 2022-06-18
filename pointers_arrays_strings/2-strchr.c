e082cd81f80285a7c7a7af483ced75a426773c77
TotiFlame <santifer4321@gmail.com>
2022-06-17 20:21:36 -0700

    commit

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character in a string
+* @s: char pointer
+* @c: char
+* Return: char
+*/
+
+char *_strchr(char *s, char c)
+{
+	int loc;
+
+	for (loc = 0; s[loc]; loc++)
+	{
+		if (s[loc] == c)
+		{
+			return (s)
+		}
+	}
+	if (s[loc] == '\0')
+	{
+		return (NULL);
+	}
+}
