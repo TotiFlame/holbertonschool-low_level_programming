81660194aac2c8dcf3c2b75b1d7bfe965657d9cf
TotiFlame <santifer4321@gmail.com>
2022-06-17 22:54:09 -0700

    up to date

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _strpbk - searches a string for any of a set of bytes
+* _strpbrk - searches a string for any of a set of bytes
 * @s: char pointer
 * @accept: char pointer
 * Return: char
