1d6a7dde02d45e91c5133788dfc2e4fc04bab44d
TotiFlame <santifer4321@gmail.com>
2022-06-17 18:49:55 -0700

    up to date

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _memset - fills memory with a constant byte
+* @s: char pointer
+* @b: char
+* @n: unsigned int
+* Return: char
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int cont;
+	for (cont = 0; cont <= n && s[cont]; cont++)
+	{
+		s[cont] = b;
+	}
+	return (s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -39,4 +39,6 @@ char *cap_string(char *);
 
 char *leet(char *);
 
+char *_memset(char *s, char b, unsigned int n);
+
 #endif
