268b2ef4799ab41e838f84491c2d82b5ed8e41dd
TotiFlame <santifer4321@gmail.com>
2022-06-17 23:10:02 -0700

    up to date

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int scont;
+	int acont;
+	int cant;
+	unsigned int res;
+
+	for (cant = 0; accept[cant] != '\0'; cant++)
+	{
+	}
+
+	for (scont = 0; scont < cant; scont++)
+	{
+		for (acont = 0; accept[acont]; acont++)
+		{
+			if (s[scont] == accept [acont])
+			{
+				res++;
+			}
+		}
+	}
+	return (res);
+}
