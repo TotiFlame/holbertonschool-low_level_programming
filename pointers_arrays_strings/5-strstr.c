61b5ebe654ad237086f50072322873ad4c8dd090
TotiFlame <santifer4321@gmail.com>
2022-06-19 10:43:28 -0700

    up to date

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+* _strstr - locate a substring
+* @haystack: char pointer
+* @needle: char pointer
+* Return: char
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int cantn = 0;
+	int hcont = 0;
+	int ncont = 0;
+	int res = 1;
+	int contr = 0;
+
+	while (needle[cantn] != '\0')
+	{
+		cantn++;
+	}
+
+	while (haystack[hcont])
+	{
+		if (haystack[hcont] == needle[ncont] && haystack[hcont] != '\0')
+		{
+			contr = 1;
+			while (contr <= cantn - 1)
+			{
+				if (haystack[hcont] == needle[ncont])
+				{
+					hcont++;
+					ncont++;
+					contr++;
+					res++;
+				}
+			}
+		}
+		hcont++;
+	}
+	if (res == cantn)
+	{
+		return (&needle[0]);
+	}
+	return ('\0');
+}
