920a6859302ac0d15227daf15a8f6c83b74e0cab
TotiFlame <santifer4321@gmail.com>
2022-06-20 09:43:00 -0700

    commit

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
-*
-*
-*
-*
+* _strlen_recursion - return lenght of a string
+* @s: char pointer
+* Retrun: int
 */
 
 int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
-		_strlen_recursion(s + 1);
 		return (1 + _strlen_recursion(s + 1));
 	}
 
