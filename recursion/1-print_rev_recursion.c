0bda75142674baf85c2e8cd865c16a66449c998b
TotiFlame <santifer4321@gmail.com>
2022-06-20 08:23:04 -0700

    up to date

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - pritns rev
+* @s: char pointer
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (s[0] == '\0')
+		_putchar('\n');
+
+	if (*s != '\0')
+	{
+		s++;
+		if (*s != '\0')
+		{
+			_print_rev_recursion(s);
+			_putchar(*s);
+		}
+	}
+}
