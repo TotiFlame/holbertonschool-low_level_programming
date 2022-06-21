8158ee595e8a9ac6c01e435f63949a3a2a7fb631
TotiFlame <santifer4321@gmail.com>
2022-06-21 09:06:41 -0700

    up to date

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main -  prints its name
+* @argc: integer
+* @argv: char pointer to array
+* Return: int
+*/
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
