0a07fbaabd8b3e898a94c68520a00d5521399ee6
TotiFlame <santifer4321@gmail.com>
2022-06-29 11:05:39 -0700

    commit

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (name[0] == '\0' || f == NULL)
 		return;
 	f(name);
 }
