d72f88df6f1740d27e7f71fb1a7d2e6c3db95644
TotiFlame <santifer4321@gmail.com>
2022-07-15 11:16:43 -0700

    insert node

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newnode;
 	unsigned int r = 0;
 
-	if (!(*head)->next)
+	if (!(*head) || !(*head)->next)
 		return (NULL);
 	newnode = malloc(sizeof(listint_t));
 	if (!newnode)
