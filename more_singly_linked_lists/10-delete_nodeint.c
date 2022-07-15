3455c8e64641e8a5a5556037a561a8a1eedcf348
TotiFlame <santifer4321@gmail.com>
2022-07-15 12:49:59 -0700

    delete index

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -10,31 +10,32 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *tmp;
 	listint_t *del;
-	unsigned int r = 0;
+	unsigned int r = 0, i = 0;
 
 	if (!(*head))
 		return (-1);
+	tmp = *head;
 	if (index == 0)
 	{
-		tmp = *head;
 		*head = (*head)->next;
 		free(tmp);
 		return (1);
 	}
-	tmp = *head;
-	while (tmp || r != (index - 1))
-	{
-		tmp = tmp->next;
-		r++;
-	}
-	r = 0;
 	del = *head;
 	while (del || r != index)
 	{
 		del = del->next;
 		r++;
 	}
-	tmp->next = del->next;
-	free(del);
+	while (tmp || i != (index - 1))
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (del->next == NULL)
+	{
+		free(del);
+		tmp->next = NULL;
+	}	
 	return (1);
 }
