2081298de98eef09212b2ceae15cdc3d547c63a1
TotiFlame <santifer4321@gmail.com>
2022-07-15 13:19:03 -0700

    delete index

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	while (del || r != index)
 	{
 		if (!del->next)
-			return (1);
+			return (-1);
 		del = del->next;
 		r++;
 	}
