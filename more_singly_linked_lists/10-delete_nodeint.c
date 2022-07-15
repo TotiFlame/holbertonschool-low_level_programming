a25ea96a11df977bbdfee7afa75aa833df9841ee
TotiFlame <santifer4321@gmail.com>
2022-07-15 12:25:50 -0700

    delete node

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+*
+*
+*
+*
+*
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp;
+	listint_t *del;
+	unsigned int r = 0;
+
+	if (!(*head))
+		return (-1);
+	tmp = *head;
+	while (tmp || r != (index - 1))
+	{
+		tmp = tmp->next;
+		r++;
+	}
+	r = 0;
+	del = *head;
+	while (del || r != index)
+	{
+		del = del->next;
+		r++;
+	}
+	tmp->next = del->next;
+	free(del);
+	return (1);
+}
