f13393d4f87a2bb87b9d8686358cac54ebc0b7a2
TotiFlame <santifer4321@gmail.com>
2022-07-19 10:48:57 -0700

    bit to 0

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* clear_bit - sets the value of a bit
+* to 0 at a given index
+* @n: decimal number
+* @index: index
+* Return: 1 or -1
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i = 0;
+	unsigned long int res = 1;
+
+	if (index > 64)
+		return (-1);
+	if(((*n >> index) & 1) == 1)
+	{
+		for (i = 0; i < index; i++)
+		{
+			res = (res * 2);
+		}
+		*n = *n - res;
+		return (1);
+	}
+	return (-1);
+}
