4da4cd3399caf96bec73ebeaecfdcc131da2cb55
TotiFlame <santifer4321@gmail.com>
2022-07-20 13:28:58 -0700

    appends text

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,13 +10,13 @@ int append_text_to_file(const char *filename, char *text_content)
 {
     int fd;
 
-    if (!*filename)
-        return (-1);
-    fd = open(filename, O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
-    if (text_content)
-        write(fd, text_content, strlen(text_content));
+	if (!*filename)
+		return (-1);
+	fd = open(filename, O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+		write(fd, text_content, strlen(text_content));
 
-    return (1);
+	return (1);
 }
