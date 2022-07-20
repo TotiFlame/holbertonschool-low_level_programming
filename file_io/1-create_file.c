5bf7f77dc6295e927821df478b0645f3732e2114
TotiFlame <santifer4321@gmail.com>
2022-07-20 13:11:11 -0700

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 	if (text_content)
 		write(fd, text_content, strlen(text_content));
 
