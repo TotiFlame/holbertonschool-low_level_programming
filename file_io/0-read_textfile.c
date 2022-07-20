5ca0d0187b244f62878ab3efab3ea6ab19cd0b86
TotiFlame <santifer4321@gmail.com>
2022-07-20 09:36:44 -0700

    read text

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-*
-*
-*
-*
-*
+* read_textfile - reads a text file
+* and prints it
+* @filename: file name
+* @letters: number of letters
+* Return: number of letters printed
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
