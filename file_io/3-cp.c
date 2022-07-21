5376a76b03b54e59769fa0aaf786115e0e500023
TotiFlame <santifer4321@gmail.com>
2022-07-20 18:25:31 -0700

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -32,26 +32,14 @@ int main(int argc, char *argv[])
 	while (lenght)
 	{
 		if (write(file_to, bread, lenght) != lenght)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 		lenght = read(file_from, bread, 1024);
 		if (lenght == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+			dprintf(2, "Error: Can't read from file %s\n", argv[1], exit(98));
 	}
 	if (close(file_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", file_from), exit(100);
 	if (close(file_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file_to);
-        exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", file_to), exit(100);
 	return (0);
 }
