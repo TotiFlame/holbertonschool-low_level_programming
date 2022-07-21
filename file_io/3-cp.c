cfa93003592e31b1aaa4b586b2d1d866735b3072
TotiFlame <santifer4321@gmail.com>
2022-07-20 18:15:48 -0700

    cp

---
+++ file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include <stddef.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int lenght, file_from, file_to;
+	char bread[1024];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	lenght = read(file_from, bread, 1024);
+	while (lenght)
+	{
+		if(write(file_to, bread, lenght) != lenght)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+		lenght = read(file_from, bread, 1024);
+		if (lenght == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+	}
+	if (close(file_from) == -1)
+	{
+		dprintf("Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf("Error: Can't close fd %d\n", file_to);
+        exit(100);
+	}
+	return (0);
+}
