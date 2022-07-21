bedbc8e627cffce4c89f50f3c6b8388df70cba4a
TotiFlame <santifer4321@gmail.com>
2022-07-21 07:24:12 -0700

    add dnode int

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode;
 
-	newnode = malloc(sizeof(listint_t));
+	newnode = malloc(sizeof(dlistint_t));
 	if (!newnode)
 		return (NULL);
 
