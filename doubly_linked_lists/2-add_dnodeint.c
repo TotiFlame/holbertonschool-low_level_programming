39fa96029a56e91ce21ac173cd2365d3dcd72e66
TotiFlame <santifer4321@gmail.com>
2022-07-21 08:35:05 -0700

    add dnode

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -25,6 +25,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		newnode->n = n;
         newnode->prev = NULL;
         newnode->next = *head;
+		(*head)->prev = newnode;
 	}
 	*head = newnode;
 	return (*head);
