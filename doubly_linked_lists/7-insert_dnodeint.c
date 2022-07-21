a24fc79f88c36566e6e1ebbdac5a9af59609e860
TotiFlame <santifer4321@gmail.com>
2022-07-21 14:19:37 -0700

    insert node at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,8 +13,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *newnode;
 	unsigned int i = 0;
 
-	if (!(*h))
-		return (NULL);
 	newnode = malloc(sizeof(dlistint_t));
 	if (!newnode)
 		return (NULL);
