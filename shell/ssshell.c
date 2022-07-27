dd5f46fe8e68ec49d11c41236285415ca6d24790
TotiFlame <santifer4321@gmail.com>
2022-07-26 18:50:05 -0700

    commit

--- shell/ssshell.c
+++ shell/ssshell.c
@@ -2,5 +2,30 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
 
-int
+int main(void)
+{
+	char *buffer;
+	size_t buffsize = 0;
+	int characters;
+	char *argv[] = {"", NULL};
+
+	buffer = malloc(buffsize);
+	if (!buffer)
+	{
+		return (0);
+	}
+	printf("#cisfun$ ");
+	characters = getline(&buffer, &buffsize, stdin);
+	strtok(buffer, "\n");
+	printf("%s\n", buffer);
+	argv[0] = buffer;
+	if (execve(argv[0], argv, NULL) == -1)
+	{
+		perror("Error:");
+		return (0);
+	}
+	return (0);
+}
