264e091e54fa77ba5b024c1e86f6fa2ee78880f4
TotiFlame <santifer4321@gmail.com>
2022-08-20 10:19:18 -0700

    Repository cloned file .h created first exercise created empty

---
+++ hash_table/0-hash_table_create.c
@@ -0,0 +1,11 @@
+#include "hash_table.h"
+
+/**
+* hash_table_create - this function creates a hash table
+* @size: size of the table
+* Return: hash_table_t
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	
+}
