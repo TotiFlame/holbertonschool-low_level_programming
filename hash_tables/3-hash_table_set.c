c2c76efc73ca1b8707dcf89c125610b03bbb44af
TotiFlame <santifer4321@gmail.com>
2022-08-21 12:07:14 -0700

    Betty style exercise 3 fixed

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,10 +15,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *valuedup = strdup(value);
 	char *keydup = strdup(key);
 	unsigned int index = 0;
-	
+
 	if (!ht)
 		return (0);
-
 	index = key_index((unsigned char *)key, ht->size);
 	node = (hash_node_t *)malloc(sizeof(hash_node_t));
 	node->key = keydup;
