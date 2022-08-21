7672c29a100a303e6058255e06ec565001227314
TotiFlame <santifer4321@gmail.com>
2022-08-21 09:04:55 -0700

    First exercise finished

--- hash_table/0-hash_table_create.c
+++ hash_table/0-hash_table_create.c
@@ -3,9 +3,21 @@
 /**
 * hash_table_create - this function creates a hash table
 * @size: size of the table
-* Return: hash_table_t
+* Return: a pointer to the newly hash_table_t
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(sizeof((hash_node_t *) * size));
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
 }
