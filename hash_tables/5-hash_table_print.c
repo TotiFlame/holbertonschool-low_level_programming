d6ecefa7d76d3ce7430c874baea55c2b662fef25
TotiFlame <santifer4321@gmail.com>
2022-08-21 19:19:05 -0700

    Exercise 5 first attempt

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+* hash_table_print - Prints a hash table
+*
+* @ht: Is the hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0, j = 0;
+	hash_node_t *node;
+	
+	if (!ht)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i])
+		{	
+			if (j != 0)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			node = ht->array[i]->next;
+			while (node)
+			{
+				printf(", ");
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("}\n");
+}
