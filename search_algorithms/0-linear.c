8b63ede099e8692ebf618ae1c1e4a4640958a5aa
TotiFlame <santifer4321@gmail.com>
2022-08-25 12:22:26 -0700

    Exercise 0 fixed second attempt

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -18,6 +18,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	while (i < size)
 	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 		i++;
