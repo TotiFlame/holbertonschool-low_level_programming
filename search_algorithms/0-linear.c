9db58cb6653a07b4ca531386e34006e0580be4a9
TotiFlame <santifer4321@gmail.com>
2022-08-25 12:18:30 -0700

    .h file created and also exercise 0 first attempt

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+/**
+* linear_search - searches for a value in
+* a array of integers using linear search algorithm
+*
+* @array: is a pointer to the first element of the array
+* @size: is the number of elements in array
+* @value: is the value to search for
+*
+* Return: -1 if is not present or if the array is NULL
+* or the first index where value is located
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i = 0;
+
+	if (!array)
+		return (-1);
+	while (i < size)
+	{
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
