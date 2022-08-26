1cea681c12633742ba72d5a426c86ec36d77a439
TotiFlame <santifer4321@gmail.com>
2022-08-26 09:55:41 -0700

    Exercise 1 first attempt

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+/**
+* binary_search - searches for a value in a sorted array
+* of integers using the binary search algorithm
+*
+* @array: is a pointer to the first element of the array
+* @size: is the number of elements in array
+* @value: is the value to search for
+*
+* Return: -1 if is not present or if the array is NULL
+* or the first index where value is located
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	unsigned int i, div = 0, min = 0, max;
+	
+	if (!array)
+		return (-1);
+	max = size - 1;
+	while (min < max)
+	{
+		i = min;
+		printf("Searching in array:");
+		while (i <= max)
+		{
+			if (i == max)
+				printf(" %d\n", array[i]);
+			else
+				printf(" %d,", array[i]);
+			i++;
+		}
+		div = (min + max) / 2;
+		if (value > array[div])
+			min = div + 1;
+		else if (value < array[div])
+			max = div - 1;
+		else
+			return (array[div]);
+	}
+	return (-1);
+}
