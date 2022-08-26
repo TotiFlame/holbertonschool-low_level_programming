ac26c94abedf19d50f5a03e58b3fd931eb5e5929
TotiFlame <santifer4321@gmail.com>
2022-08-26 10:38:15 -0700

    Exercise 1 second attempt

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -17,25 +17,25 @@ int binary_search(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 	max = size - 1;
-	while (min < max)
+	while (min <= max)
 	{
-		i = min;
-		printf("Searching in array:");
-		while (i <= max)
-		{
-			if (i == max)
-				printf(" %d\n", array[i]);
-			else
-				printf(" %d,", array[i]);
-			i++;
-		}
 		div = (min + max) / 2;
+		i = min;
+        printf("Searching in array:");
+        while (i <= max)
+        {
+            if (i == max)
+                printf(" %d\n", array[i]);
+            else
+                printf(" %d,", array[i]);
+            i++;
+        }
 		if (value > array[div])
 			min = div + 1;
-		else if (value < array[div])
+		if (value < array[div])
 			max = div - 1;
-		else
-			return (array[div]);
+		if (value == array[div])
+			return (div);
 	}
 	return (-1);
 }
